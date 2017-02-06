5f14fbfc6c222686ee8fb8014954c469ed4c89c9
SketchyRandom <sketchyrandom@gmail.com>
2017-02-06 13:19:38 +1100

    Added support for gameroom.js

--- guildmanager.js
+++ guildmanager.js
@@ -6,26 +6,100 @@ var txtChnlGR2;
 var voiceChnlGR1;
 var voiceChnlGR2;
 
-function findChannels(guild){
+var roleAdmin;
+var roleGM;
+var inGame1;
+var inGame2;
+
+var gameRoles = [];
+var gameTxtChnls = [];
+var gameVoiceChnls = [];
+
+function initialise(guild){
     txtChnlWelcome = guild.generalChannel;
     txtChnlWelcome = guild.textChannels.find(c => c.name == "welcome");
     txtChnlGR1 = guild.textChannels.find(c => c.name == "gameroom_1");
+    gameTxtChnls.push(txtChnlGR1);
     txtChnlGR2 = guild.textChannels.find(c => c.name == "gameroom_2");
+    gameTxtChnls.push(txtChnlGR2);
 
     voiceChnlGR1 = guild.voiceChannels.find(c => c.name == "GameRoom #1");
-    voiceChnlGR1 = guild.voiceChannels.find(c => c.name == "GameRoom #2");
-    console.log("Channels logged.");
+    gameVoiceChnls.push(voiceChnlGR1);
+    voiceChnlGR2 = guild.voiceChannels.find(c => c.name == "GameRoom #2");
+    gameVoiceChnls.push(voiceChnlGR2);
+/* - console clutter 
+    console.log("Game channels found: \n");
+    console.log(getGameRooms());
+    console.log("\nChannels logged.\n\n");
+*/
+    roleAdmin = guild.roles.find(r => r.name == "Admin");
+    roleGM = guild.roles.find(r => r.name == "Game Manager");
+    inGame1 = guild.roles.find(r => r.name == "In Game #1");
+    gameRoles.push(inGame1);
+    inGame2 = guild.roles.find(r => r.name == "In Game #2");
+    gameRoles.push(inGame2);
+/*
+    console.log("Game roles found: \n")
+    console.log(getGameRoles());
+    console.log("\nRoles logged.\n\n");
+*/
+}
+
+function addRole(player, role){
+
+}
+
+function removeRole(player, role){
+
+}
+
+
+function getGameTxtChannels() {
+    return gameTxtChnls;
+}
+
+function getGameVoiceChannels() {
+    return gameVoiceChnls;
+}
+
+function getGameRoles() {
+    return gameRoles;
+}
+
+function maxGames(){
+    max = 15;
+    if(gameTxtChnls.length <= max){
+        max = gameTxtChnls.length;
+    }
+    if(gameVoiceChnls.length <= max){
+        max = gameVoiceChnls.length;
+    }
+    if(gameRoles.length <= max){
+        max = gameRoles.length;
+    }
+    return max;
 }
 
 function listChannels() {
-    return txtChnlGR1.name;
+    var list = "";
+    if(gameTxtChnls.length < 1){
+        return "I could not find any channels."
+    }else{
+        list += "Found " + gameTxtChnls.length + " channels: \n";
+    }
+    for(var i = 0; i < gameTxtChnls.length; i++){
+        list += "\n" + gameTxtChnls[i].name;
+    }
+    return list;
 }
 
-function getGameRooms() {
-    return [txtChnlGR1, txtChnlGR2];
-}
 module.exports = {
-    findChannels : findChannels,
+    initialise : initialise,
     listChannels : listChannels,
-    getGameRooms: getGameRooms
+    getGameTxtChannels : getGameTxtChannels,
+    getGameVoiceChannels : getGameVoiceChannels,
+    getGameRoles : getGameRoles,
+    addRole : addRole,
+    maxGames : maxGames,
+    removeRole : removeRole
 }
